b2876864a1d00c60b265888e1b4627b5c2740fd3
Glitch (cooing-unmarred-xylocarp) <none>
2022-05-04 02:42:10 +0000

    🎺👂 Checkpoint ./index.js:44463898/671

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
+  if (msg.content === 'hi') {
+    msg.reply('Hi!');
+  }
+  if (msg.content === 'web') {
+    msg.reply('我的網站連結: https://willyhsu.rf.gd/');
+  }
+  if (msg.content === 'blink') {
+    msg.reply('機器邀請連結: https://discord.com/api/oauth2/authorize?client_id=904281957289054218&permissions=8&scope=bot');
+  }
+  
 });
 
 client.login(token);
