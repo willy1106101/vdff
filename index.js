d41fa5c2b869f32762e9e4240198f206cd2efc4e
Glitch (cooing-unmarred-xylocarp) <none>
2022-05-04 11:39:05 +0000

    👩‍👩‍👦💽 Checkpoint ./index.js:44463898/432

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg,reaction, user) => {
-  if (msg.content === 'ping') {
+  if (msg.content === '!ping') {
     msg.reply('Pong!');
   }
-  if (msg.content === 'hi') {
+  if (msg.content === '!hi') {
     msg.reply('Hi!');
   }
-  if (msg.content === 'web') {
+  if (msg.content === '!web') {
     msg.reply('我的網站連結: https://willyhsu.rf.gd/');
+    msg.seturl('https://willyhsu.rf.gd/')
   }
-  if (msg.content === 'blink') {
+  if (msg.content === '!blink') {
     msg.reply('機器邀請連結: https://discord.com/api/oauth2/authorize?client_id=904281957289054218&permissions=8&scope=bot');
   }
   /*const member = reaction.message.guild.members.cache.get(user.id); 
@@ -27,6 +28,13 @@ client.on('message', (msg,reaction, user) => {
   }*/
 });
 
+client.on('guildMemberAdd', (member) => {
+    const channelId = 'CHANNEL_ID'; // The Channel ID you just copied
+    const welcomeMessage = `Hey <@${member.id}>! Welcome to my server!`;
+    member.guild.channels.fetch(channelId).then(channel => {
+        channel.send(welcomeMessage)
+    });
+});
 
 /*client.on('messageReactionAdd', (reaction, user) => {
  const member = reaction.message.guild.members.cache.get(user.id);   
